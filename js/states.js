07ca4d11cdd4b7430e44169e93c3e82608f64850
brmayes <brmayes@live.unc.edu>
2016-02-12 17:02:50 -0500

    Added working ng-click function to state boxes, need to properly access the data.

--- js/states.js
+++ js/states.js
@@ -3,17 +3,37 @@ var app = angular.module('states', []);
 app.controller('StatesController', ['$http', function($http) {
 
     //pulling json data using ajax
-    this.senatorData = [];
+    this.stateSeats = [];
     var _this = this;
+
 
     $http.get('/data/senator_data.json')
         .success(function(data) {
             console.log(data);
-            console.log(this);
-            _this.players = data;
+            _this.stateSeats = data;
+
         })
         .error(function(msg) {
             console.log("This request failed.\n" + msg);
         });
+
+    this.states = function(stateName) {
+      console.log("clicked");
+
+      for (var i = 0; i < this.stateSeats.length; i++) {
+
+        if (stateName == this.stateSeats[i].state) {
+          console.log(this.stateSeats[i]);
+        }
+
+      }
+
+      // for (var i = 0; i < this.states.length; i++) {
+      //   if (stateName == this.states[i].last) {
+      //     this.currentState = this.states[i];
+      //     alert("hello");
+      //   }
+      // }
+    }
 
 }]);
