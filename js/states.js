3bb138a79cbbac654f623bfbdddd67981eca50cf
brmayes <brmayes@live.unc.edu>
2016-02-13 00:01:30 -0500

    Started structure for ng-click function

--- js/states.js
+++ js/states.js
@@ -5,6 +5,7 @@ app.controller('StatesController', ['$http', function($http) {
     //pulling json data using ajax
     this.stateSeats = [];
     var _this = this;
+    this.currentState;
 
 
     $http.get('/data/senator_data.json')
@@ -18,22 +19,32 @@ app.controller('StatesController', ['$http', function($http) {
         });
 
     this.states = function(stateName) {
-      console.log("clicked");
+      // console.log("clicked");
 
       for (var i = 0; i < this.stateSeats.length; i++) {
 
         if (stateName == this.stateSeats[i].state) {
-          console.log(this.stateSeats[i]);
+          // console.log(this.stateSeats[i].state);
+          this.currentState = this.stateSeats[i];
+          this.seatButtons = "";
+
+          for (var x = 0; x <= 1; x++) {
+            this.seatButtons += '<div class="senatorBox col-md-6 col-xs-6">';
+            this.seatButtons += '<p>Seat ' + x + "</p>";
+            this.seatButtons += this.currentState.senator_seats[x].contested;
+
+
+            this.seatButtons += '</div>';
+
+          }
+
+          document.getElementById("seatButtonsDiv").innerHTML = this.seatButtons;
+
         }
 
       }
 
-      // for (var i = 0; i < this.states.length; i++) {
-      //   if (stateName == this.states[i].last) {
-      //     this.currentState = this.states[i];
-      //     alert("hello");
-      //   }
-      // }
+
     }
 
 }]);
