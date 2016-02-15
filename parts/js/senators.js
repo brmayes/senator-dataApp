8557a8e6ccfeed3e1b95bd6521dfacadc53eb70c
brmayes <brmayes@live.unc.edu>
2016-02-14 20:24:54 -0500

    Created a function to loop through the data and match a taken variable (senator) to the data associated.

--- parts/js/senators.js
+++ parts/js/senators.js
@@ -5,7 +5,8 @@ app.controller('SenatorsController', ['$http', function($http) {
     //pulling json data using ajax
     this.stateSeats = [];
     var _this = this;
-    this.currentSeat;
+    this.currentState;
+    this.currentSenator;
 
 
     $http.get('/data/senator_data.json')
@@ -18,9 +19,40 @@ app.controller('SenatorsController', ['$http', function($http) {
             console.log("This request failed.\n" + msg);
         });
 
-    this.currentSeat = function(incumLast) {
+    this.currentSeat = function(nameOfState, incumLast) {
 
-      console.log("hello");
+      this.currentState = nameOfState;
+
+      console.log(nameOfState);
+
+      //search through fifty states
+      for (var j = 0; j < this.stateSeats.length; j++) {
+
+        //match taken variable to state data
+        if (nameOfState == this.stateSeats[j].state) {
+
+          //search through senate seats
+          for (var k = 0; k < this.stateSeats[k].senator_seats.length; k++) {
+
+            //match taken variable to senator seats data
+            if (incumLast == this.stateSeats[j].senator_seats[k].incumbent_last) {
+
+              //assign current input to senator
+              this.currentSenator = this.stateSeats[j].senator_seats[k];
+
+              //do stuff here
+              
+
+
+
+
+            }
+
+          }
+
+        }
+
+      }
 
     }
 
