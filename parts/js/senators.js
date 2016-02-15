ad96f5e4c61a776c714d97e04e800274e4f05534
brmayes <brmayes@live.unc.edu>
2016-02-14 23:01:13 -0500

    Added functionality to show a list of candidates if the seat is contested.

--- parts/js/senators.js
+++ parts/js/senators.js
@@ -7,6 +7,7 @@ app.controller('SenatorsController', ['$http', function($http) {
     var _this = this;
     this.currentState;
     this.currentSenator;
+    this.showCandidates = false;
 
 
     $http.get('/data/senator_data.json')
@@ -23,8 +24,6 @@ app.controller('SenatorsController', ['$http', function($http) {
 
       this.currentState = nameOfState;
 
-      console.log(nameOfState);
-
       //search through fifty states
       for (var j = 0; j < this.stateSeats.length; j++) {
 
@@ -40,11 +39,48 @@ app.controller('SenatorsController', ['$http', function($http) {
               //assign current input to senator
               this.currentSenator = this.stateSeats[j].senator_seats[k];
 
-              //do stuff here
-              
+              this.currentSenatorData = "";
+              this.currentSeatContested = "";
+              this.currentSeatParty = "";
+              this.currentCandidatesData = "";
+
+              // "img": "imgs/senators/shelby_richard.jpg",
+              // "years_in_off": 29,
+
+              if (this.currentSenator.contested == true) {
+                this.currentSeatContested = "Yes"
+              } else if (this.currentSenator.contested == false) {
+                this.currentSeatContested = "No"
+              }
+
+              if (this.currentSenator.cur_party == "Republican") {
+                this.currentSeatParty = "R"
+              } else if (this.currentSenator.cur_party == "Democratic") {
+                this.currentSeatParty = "D"
+              }
+
+              //print stuff here
+              this.currentSenatorData += '<h4>' + this.currentSenator.incumbent_first + ' ' + this.currentSenator.incumbent_last + ', ' + this.currentSeatParty + '</h4>';
+              this.currentSenatorData += '<div class="row"><div class="col-md-6"><p>Contested: ' + this.currentSeatContested + '</div><div class="col-md-6"><p>Term End: ' + this.currentSenator.term_end + '</div></div>';
+
+              if (this.currentSenator.candidates[0] != null ) {
+                this.showCandidates = true;
+                this.candidatesList = this.currentSenator.candidates;
+
+                for (var k = 0; k < this.currentSenator.candidates.length; k++) {
+                  this.currentCandidatesData += '<li>' + this.candidatesList[k].name + '</li>';
+                }
+
+              } else {
+                console.log("no candidates")
+              }
 
 
 
+              //this.currentCandidatesData += '';
+
+              document.getElementById("selectedIncumDiv").innerHTML = this.currentSenatorData;
+              document.getElementById("selectedCandidDiv").innerHTML = ('<ul>') + this.currentCandidatesData + ('</ul>');
 
             }
 
