73321a511070892ab28d53149d9cba3326f62c80
brmayes <brmayes@live.unc.edu>
2016-02-19 01:02:56 -0500

    FINALLY got a semi-functional filter. Trying to add two more levels to filter with.

--- js/app.js
+++ js/app.js
@@ -26,30 +26,48 @@ app.controller('BaseController', function() {
 });
 
 app.filter('stateFilter', function() {
-  return function(input, contested, notContested, democratic, republican, independent) {
+  return function(input, contested, democratic, republican, independent) {
+
+    console.log(contested);
 
     var results = [];
 
     for (var i in input) {
       var match = false;
 
-      if (contested === true && input[i].contested === true) {
-        match = true;
-      }
-
-      if (notContested === true && input[i].contested === false) {
-        match = true;
-      }
-
-      if (republican === true && input[i].cur_party == 'Republican') {
-        match = true;
-      }
-
-      if (democratic === true && input[i].cur_party === 'Democratic') {
-        match = true;
-      }
-
-      if (independent === true && input[i].cur_party === 'Independent') {
+      if (contested == "true" && input[i].contested == true) {
+        if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+          match = true;
+        } else if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic')) {
+          match = true;
+        } else if ((republican === true && input[i].cur_party == 'Republican') && (independent === true && input[i].cur_party === 'Independent')) {
+          match = true;
+        } else if ((democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+          match = true;
+        } else if (republican === true && input[i].cur_party == 'Republican') {
+          match = true;
+        } else if (democratic === true && input[i].cur_party == 'Democratic') {
+          match = true;
+        } else if (independent === true && input[i].cur_party == 'Independent') {
+          match = true;
+        }
+      } else if (contested == "false" && input[i].contested == false) {
+          if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+            match = true;
+          } else if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic')) {
+            match = true;
+          } else if ((republican === true && input[i].cur_party == 'Republican') && (independent === true && input[i].cur_party === 'Independent')) {
+            match = true;
+          } else if ((democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+            match = true;
+          } else if (republican === true && input[i].cur_party == 'Republican') {
+            match = true;
+          } else if (democratic === true && input[i].cur_party == 'Democratic') {
+            match = true;
+          } else if (independent === true && input[i].cur_party == 'Independent') {
+            match = true;
+          }
+      } else if (contested == "true" && input[i].contested == true) {
         match = true;
       }
 
@@ -59,8 +77,172 @@ app.filter('stateFilter', function() {
 
     console.log(results)
 
-    if (contested === undefined && notContested === undefined && republican === undefined && democratic === undefined && independent === undefined) results = input;
+    if ((contested == undefined) && (republican == undefined || republican == false) && (democratic == undefined || democratic == false) && (independent == undefined || independent == false)) results = input;
 
     return results;
   };
 })
+
+
+// app.filter('stateFilter', function() {
+//   return function(input, contested, notContested, democratic, republican, independent) {
+//
+//     // console.log(contested)
+//     //
+//     // console.log(notContested)
+//
+//     var results = [];
+//
+//     for (var i in input) {
+//       var match = false;
+//
+//       console.log(input[i].contested);
+//
+//       if (contested === true && input[i].contested === true) {
+//         console.log("true!!")
+//         match = true;
+//       }
+//
+//       if (notContested === true && input[i].contested === false) {
+//         match = true;
+//       }
+//
+//       if (republican === true && input[i].cur_party === 'Republican') {
+//         match = true;
+//       }
+//
+//       if (democratic === true && input[i].cur_party === 'Democratic') {
+//         match = true;
+//       }
+//
+//       if (independent === true && input[i].cur_party === 'Independent') {
+//         match = true;
+//       }
+//
+//       if (match) results.push(input[i]);
+//
+//     }
+//
+//     console.log(results)
+//
+//     if ((republican == undefined || republican == false) && (democratic == undefined || democratic == false) && (independent == undefined || independent == false)) results = input;
+//
+//     return results;
+//   };
+// })
+
+
+// app.filter('stateFilter', function() {
+//   // return function(input, contested, notContested, democratic, republican, independent) {
+//
+//   return function(input, contested, democratic, republican, independent) {
+//
+//     var results = [];
+//
+//     for (var i in input) {
+//       var match = false;
+//
+//       if (contested === true && input[i].contested === true) {
+//         match = true;
+//         // if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+//         //   match = true;
+//         // } else if ((republican === true && input[i].cur_party == 'Republican') && (democratic === true && input[i].cur_party === 'Democratic')) {
+//         //   match = true;
+//         // } else if ((republican === true && input[i].cur_party == 'Republican') && (independent === true && input[i].cur_party === 'Independent')) {
+//         //   match = true;
+//         // } else if ((democratic === true && input[i].cur_party === 'Democratic') && (independent === true && input[i].cur_party === 'Independent')) {
+//         //   match = true;
+//         // } else if (republican === true && input[i].cur_party == 'Republican') {
+//         //   match = true;
+//         // } else if (democratic === true && input[i].cur_party == 'Democratic') {
+//         //   match = true;
+//         // } else if (independent === true && input[i].cur_party == 'Independent') {
+//         //   match = true;
+//         // }
+//       }
+//
+//       //FILTERS FOR CONTESTED
+//
+//       // if (contested === true && input[i].contested === true) {
+//       //     if (republican === true && input[i].cur_party == 'Republican') {
+//       //
+//       //       console.log("both true")
+//       //       match = true;
+//       //
+//       //     } else {
+//       //
+//       //       console.log("one true")
+//       //       match = true;
+//       //
+//       //     }
+//       // }
+//
+//
+//       // if (contested === true && input[i].contested === true) {
+//       //   if (democratic === true && input[i].cur_party == 'Democratic') {
+//       //     match = true;
+//       //   }
+//       // }
+//       //
+//       // if (contested === true && input[i].contested === true) {
+//       //   if (independent === true && input[i].cur_party == 'Independent') {
+//       //     match = true;
+//       //   }
+//       // }
+//
+//       // //FILTERS FOR UNCONTESTED
+//       // if (notContested === true && input[i].contested === false) {
+//       //   if (republican === true && input[i].cur_party == 'Republican') {
+//       //     match = true;
+//       //   }
+//       // }
+//       //
+//       // if (notContested === true && input[i].contested === false) {
+//       //   if (democratic === true && input[i].cur_party == 'Democratic') {
+//       //     match = true;
+//       //   }
+//       // }
+//       //
+//       // if (notContested === true && input[i].contested === false) {
+//       //   if (independent === true && input[i].cur_party == 'Independent') {
+//       //     match = true;
+//       //   }
+//       // }
+//       //
+//       // //ONE FILTER
+//       // if (contested === true && input[i].contested === true) {
+//       //   match = true;
+//       // }
+//       //
+//       // if (notContested === true && input[i].contested === false) {
+//       //   match = true;
+//       // }
+//
+//       // if (republican === true && input[i].cur_party == 'Republican') {
+//       //   match = true;
+//       // }
+//       //
+//       // if (democratic === true && input[i].cur_party === 'Democratic') {
+//       //   match = true;
+//       // }
+//       //
+//       // if (independent === true && input[i].cur_party === 'Independent') {
+//       //   match = true;
+//       // }
+//
+//       if (match) results.push(input[i]);
+//
+//     }
+//
+//
+//     // console.log(results)
+//
+//     // if (contested === undefined && notContested === undefined && republican === undefined && democratic === undefined && independent === undefined) results = input;
+//     // if (republican == false && democratic == false && independent == false) results = input;
+//     if ((republican == undefined || republican == false) && (democratic == undefined || democratic == false) && (independent == undefined || independent == false)) results = input;
+//
+//
+//
+//     return results;
+//   };
+// })
